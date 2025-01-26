251c4c6e03cd09bb7f778db7b1c334e5618e3316
ubungmeister <ubungmeister@gmail.com>
2025-01-26 15:12:26 +0100

    base_url added

--- app/cars/page.tsx
+++ app/cars/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { CarList } from './components/CarList';
-
-const data = async() => {
-    const res = await fetch('http://localhost:3000/api/cars');
+ 
+const data = async () => {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL  
+    const res = await fetch(`${baseUrl}/api/cars`);
+    if (!res.ok) throw new Error('Failed to fetch data');
     const data = await res.json();
     return data;
-}
-
+};
 const page = async() => {
 
     try{
